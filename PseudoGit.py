e7a43fa0713515e2abb79ea35abf411f7d3fe6d0
gorkemsolun <gorkemkadir14@hotmail.com>
2024-11-02 22:45:39 +0300

    large file download

--- PseudoGit.py
+++ PseudoGit.py
@@ -37,6 +37,7 @@ import chardet
 # Defining the constants
 MAX_THREAD_COUNT = 4
 GITHUB_API = "api.github.com"
+GITHUB_API_RAW = "raw.githubusercontent.com"
 GITHUB_HOST = "github.com"
 GITHUB_PORT = 443
 BUFFER_SIZE = 4096
@@ -133,6 +134,120 @@ def send_request(secure_socket, host, port, request):
     }
 
 
+def get_file_from_github(file_name, directory="pseudo_git_downloads", parallel_count=4):
+    """
+    Function to get a file from GitHub
+
+    :param file_name: The name of the file
+    :param directory: The directory to save the file
+    :param parallel_count: The number of parallel threads to download the file
+    :param file_name: The name of the output file
+
+    :return: None
+    """
+
+    # check if the directory exists
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Create a secure socket
+    secure_socket = create_secure_socket()
+
+    # Construct the request
+    request = f"GET /repos/{username}/{repository}/contents/{file_name} HTTP/1.1\r\n"
+    request += f"Host: {GITHUB_API}\r\n"
+    request += f"Authorization: token {access_token}\r\n"
+    request += "User-Agent: PseudoGit\r\n"
+    request += "Accept: application/vnd.github.v3+json\r\n"
+    request += "Connection: close\r\n\r\n"
+
+    # Send the request and receive the response
+    response = send_request(secure_socket, GITHUB_API, GITHUB_PORT, request)
+
+    # Close the socket
+    secure_socket.close()
+
+    # Parse the response
+    response_body = json.loads(response["response_body"])
+    content = response_body["content"]
+
+    # If the response_body is not empty write the content to the file
+    if content:
+        content = base64.b64decode(content)
+        with open(f"{directory}/{file_name}", "wb") as file:
+            file.write(content)
+
+        return
+
+    # Get the file size and the download URL
+    file_size = response_body["size"]
+    download_url = response_body["download_url"]
+
+    # Calculate the chunk size
+    chunk_size = file_size // parallel_count
+
+    # Create the threads
+    threads = []
+    for i in range(parallel_count):
+        start = i * chunk_size
+        end = (i + 1) * chunk_size if i < parallel_count - 1 else file_size
+        thread = threading.Thread(
+            target=download_file_chunk,
+            args=(download_url, start, end, f"{file_name}_{i}", directory),
+        )
+        threads.append(thread)
+        thread.start()
+
+    # Wait for the threads to finish
+    for thread in threads:
+        thread.join()
+
+    # Concatenate the chunks
+    with open(f"{directory}/{file_name}", "wb") as file:
+        for i in range(parallel_count):
+            with open(f"{directory}/{file_name}_{i}", "rb") as chunk_file:
+                file.write(chunk_file.read())
+
+    # Remove the chunk files
+    for i in range(parallel_count):
+        os.remove(f"{directory}/{file_name}_{i}")
+
+
+def download_file_chunk(url, start, end, file_name, directory):
+    """
+    Function to download a file chunk
+
+    :param url: The URL of the file
+    :param start: The start byte of the chunk
+    :param end: The end byte of the chunk
+    :param file_name: The name of the output file
+    :param directory: The directory to save the file
+    """
+
+    # Create a secure socket
+    secure_socket = create_secure_socket(GITHUB_API_RAW)
+
+    # Construct the request
+    request = f"GET {url} HTTP/1.1\r\n"
+    request += f"Host: {GITHUB_API_RAW}\r\n"
+    request += "User-Agent: PseudoGit\r\n"
+    request += f"Range: bytes={start}-{end}\r\n"
+    request += "Connection: close\r\n\r\n"
+
+    # Send the request and receive the response
+    response = send_request(secure_socket, GITHUB_API_RAW, GITHUB_PORT, request)
+
+    # Close the socket
+    secure_socket.close()
+
+    # Parse the response
+    response_body = response["response_body"]
+
+    # Write the content to the file
+    with open(f"{directory}/{file_name}", "wb") as file:
+        file.write(response_body.encode())
+
+
 def get_repository_contents():
     """
     Function to get the contents of the repository
@@ -175,87 +290,7 @@ def get_repository_contents():
     return files
 
 
-def get_file_from_github(file_name):
-    """
-    Function to get a file from GitHub
-
-    :param file_name: The name of the file to get
-
-    :return: The content of the file and whether it is binary or not
-    """
-
-    if not access_token:
-        get_access_token()
-
-    if not username:
-        get_username()
-
-    if not repository:
-        get_repository()
-
-    # Create a secure socket
-    secure_socket = create_secure_socket()
-
-    # Construct the request
-    request = f"GET /repos/{username}/{repository}/contents/{file_name} HTTP/1.1\r\n"
-    request += f"Host: {GITHUB_API}\r\n"
-    request += f"Authorization: token {access_token}\r\n"
-    request += "User-Agent: PseudoGit\r\n"
-    request += "Accept: application/vnd.github.v3+json\r\n"
-    request += "Connection: close\r\n\r\n"
-
-    # Send the request and receive the response
-    response = send_request(secure_socket, GITHUB_API, GITHUB_PORT, request)
-
-    # Close the socket
-    secure_socket.close()
-
-    # Parse the response
-    response_body = json.loads(response["response_body"])
-
-    # Get the content of the file and decode it
-    try:
-        is_binary = False
-        raw_content = base64.b64decode(response_body["content"])
-        detected_encoding = chardet.detect(raw_content)["encoding"]
-
-        if detected_encoding:
-            content = raw_content.decode(detected_encoding)
-        else:  # if the encoding is not detected just write the raw content
-            content = raw_content
-            is_binary = True
-
-        return {"content": content, "is_binary": is_binary}
-    except UnicodeDecodeError:
-        raw_content = response_body["content"]
-        print("UnicodeDecodeError:", raw_content)
-
-    return None
-
-
-def download_file(file_name, prefix="pseudo_git_downloads"):
-    """
-    Function to download a file from GitHub
-
-    :param file_name: The name of the file to download
-    """
-
-    # Create the directory if it does not exist
-    if not os.path.exists(prefix):
-        os.makedirs(prefix)
-
-    file_data = get_file_from_github(file_name)
-    how_to_write = "wb" if file_data["is_binary"] else "w"
-
-    print(how_to_write)
-    print(file_name)
-
-    # Write the content to the file
-    with open(f"{prefix}/{file_name}", how_to_write) as file:
-        file.write(file_data["content"])
-
-
-def download_files(files, prefix="pseudo_git_downloads"):
+def download_files(files, directory="pseudo_git_downloads"):
     """
     Function to download files from GitHub
 
@@ -265,7 +300,7 @@ def download_files(files, prefix="pseudo_git_downloads"):
     threads = []
 
     for file in files:
-        thread = threading.Thread(target=download_file, args=(file, prefix))
+        thread = threading.Thread(target=get_file_from_github, args=(file, directory))
         threads.append(thread)
         thread.start()
 
@@ -279,10 +314,10 @@ def download_files(files, prefix="pseudo_git_downloads"):
 
 
 def main():
+    # Get the repository contents and download small files
     repo_contents = get_repository_contents()
     print("Repository contents:", repo_contents)
     download_files(repo_contents)
-    print("Files downloaded successfully!")
 
 
 if __name__ == "__main__":
